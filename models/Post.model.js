c7fec81406bdc9ba32d00d0466ca842acd19b2af
tmlsn <theo.malless@gmail.com>
2022-04-19 19:24:32 +0200

    add post model

---
+++ models/Post.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const {Schema, model} = mongoose
+
+const postSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: new Date.now,
+    },
+    editedAt:{
+        type: new Date,
+    },
+    user: {
+        type: mongoose.SchemaTypes.ObjectId,
+        required: true,
+        ref:'user',
+    }
+})
