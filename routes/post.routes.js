c7c9e89dff597c680cf676590c1c00ae02eb30ab
tmlsn <theo.malless@gmail.com>
2022-04-19 20:25:43 +0200

    set up show post, edit post and delete post routes

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -21,7 +21,41 @@ router.get("/my-posts", async (req, res) => {
 });
 
 //get all posts
-router.get("all-posts", async (req, res) => {
+router.get("/all-posts", async (req, res) => {
     const allPosts = await Post.find().populate('user');
     res.status(200).json(allPosts);
-})
+});
+
+// get one post by id
+router.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    const post = await Post.findById(id);
+    res.status(200).json(post);
+  });
+  
+  // delete post by id
+  router.delete("/:id", async (req, res) => {
+    const { id } = req.params;
+    const post = await Post.findById(id);
+    if (post.user.toString() === req.jwtPayload.user._id) {
+      await Post.findByIdAndDelete(id);
+      res.status(200).json(post);
+    } else {
+      res.status(400).json("unauthorized");
+    }
+  });
+  
+  // edit post by id
+  router.put("/:id", async (req, res) => {
+    const { id } = req.params;
+    const { content } = req.body;
+    let post = await Post.findById(id);
+    if (post.user.toString() === req.jwtPayload.user._id) {
+      post.content = content;
+      post.editedAt = new Date.now
+      post = await Post.save();
+      res.status(200).json(post);
+    } else {
+      res.status(400).json("unauthorized");
+    }
+  });
