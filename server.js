f9fdce343ee26d0c737ee302ce3be473154a9e66
tmlsn <theo.malless@gmail.com>
2022-04-19 19:32:41 +0200

    add the post routes file and set up the post routes in the server file

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require('dotenv')
 dotenv.config()
+const { authenticate } = require("./middlewares/jwt.middleware");
 
 mongoose.connect(process.env.MONGO_DB_URL);
 
@@ -18,5 +19,8 @@ app.get('/', (req, res) => {
 
 const authRoutes = require("./routes/auth.routes");
 app.use('/auth', authRoutes); 
+
+const postRoutes = require("./routes/post.routes");
+app.use('/post', authenticate, postRoutes);
 
 app.listen(process.env.PORT);
