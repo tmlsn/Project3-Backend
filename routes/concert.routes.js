7d4a16e5ebf871165ec0de6ddbea283f63331f9e
Paulina Stadnik <paulina.stadnik@rover.com>
2022-04-25 12:56:54 +0200

    added geohash param to the ticketmaster API call

--- routes/concert.routes.js
+++ routes/concert.routes.js
@@ -3,13 +3,13 @@ const axios = require("axios")
 
 const router = express.Router()
 
-router.get("/concerts", async (req, res) => {
-  let apiUrl = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${process.env.TM_CONSUMER_KEY}&keyword=barcelona`
+router.get("/:geohash", async (req, res) => {
+  const { geohash } = req.params;
+  let apiUrl = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${process.env.TM_CONSUMER_KEY}&keyword=music&geoPoint=${geohash}`
   const concerts = await axios.get(apiUrl)
   console.log(concerts.data._embedded)
   res.status(200).json(concerts.data._embedded)
 });
-
 
 
 
