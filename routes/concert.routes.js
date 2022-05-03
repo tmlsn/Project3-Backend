593e2751119a12a3592448ffbf56d76a51825ac6
Paulina Stadnik <paulina.stadnik@rover.com>
2022-05-03 20:17:04 +0200

    trying to push to heroku

--- routes/concert.routes.js
+++ routes/concert.routes.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 router.get("/:geohash", async (req, res) => {
   const { geohash } = req.params;
-  let apiUrl = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${process.env.TM_CONSUMER_KEY}&keyword=music&geoPoint=${geohash}&radius=10`
+  let apiUrl = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${process.env.TM_CONSUMER_KEY}&keyword=music&geoPoint=${geohash}&radius=11`
   const concerts = await axios.get(apiUrl)
   console.log(concerts.data._embedded)
   res.status(200).json(concerts.data._embedded)
