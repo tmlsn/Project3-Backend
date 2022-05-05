110679385ddcfc0690b0616f13fd71a00c1b809f
Paulina Stadnik <paulina.stadnik@rover.com>
2022-05-05 20:05:48 +0200

    added try catch to the API call

--- routes/concert.routes.js
+++ routes/concert.routes.js
@@ -4,11 +4,15 @@ const axios = require("axios")
 const router = express.Router()
 
 router.get("/:geohash", async (req, res) => {
-  const { geohash } = req.params;
-  let apiUrl = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${process.env.TM_CONSUMER_KEY}&keyword=music&geoPoint=${geohash}&radius=10`
-  const concerts = await axios.get(apiUrl)
-  console.log(concerts.data._embedded)
-  res.status(200).json(concerts.data._embedded)
+  try {
+    const { geohash } = req.params;
+    let apiUrl = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${process.env.TM_CONSUMER_KEY}&keyword=music&geoPoint=${geohash}&radius=10`
+    const concerts = await axios.get(apiUrl)
+    res.status(200).json(concerts.data._embedded)
+    
+  } catch (error) {
+    res.status(404).json(error)
+  }
 });
 
 
